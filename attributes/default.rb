42a7dd4b183d9344fdb40403cf94b2bdaf50d66d
Jeff Byrnes <thejeffbyrnes@gmail.com>
2017-08-07 18:08:54 -0400

    Improve handling of gzippable assets
    * Better balance of compression vs. CPU
    * Gzip more assets by default

--- attributes/default.rb
+++ attributes/default.rb
@@ -11,20 +11,32 @@ override['nginx']['log_formats']          = {
 
 override['nginx']['extra_configs']['open_file_cache'] = 'max=4096'
 
+override['nginx']['gzip_comp_level'] = '5'
 override['nginx']['gzip_types'] = %w(
-  text/plain
-  text/css
-  text/javascript
+  application/atom+xml
   application/javascript
-  application/x-javascript
   application/json
-  application/xml
-  application/xhtml+xml
-  application/x-font-ttf
-  application/x-font-opentype
+  application/ld+json
+  application/manifest+json
+  application/rss+xml
+  application/vnd.geo+json
   application/vnd.ms-fontobject
+  application/x-font-ttf
+  application/x-web-app-manifest+json
+  application/xhtml+xml
+  application/xml
+  font/opentype
+  image/bmp
   image/svg+xml
   image/x-icon
+  text/cache-manifest
+  text/css
+  text/plain
+  text/vcard
+  text/vnd.rim.location.xloc
+  text/vtt
+  text/x-component
+  text/x-cross-domain-policy
 )
 
 override['nginx']['status']['port'] = '81'
