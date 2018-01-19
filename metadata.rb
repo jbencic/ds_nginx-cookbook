60a2ac61deac45f3b16293491a168b0fc9dbc6eb
Jeff Byrnes <thejeffbyrnes@gmail.com>
2018-01-19 13:16:51 -0500

    Bump to v2.0.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jeff@darksky.net'
 license          'All Rights Reserved'
 description      'Installs/Configures ds_nginx'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.1.1'
+version          '2.0.0'
 chef_version     '>= 12.11' if respond_to?(:chef_version)
 
 issues_url 'https://github.com/darkskyapp/ds_nginx-cookbook/issues'
